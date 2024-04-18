1f105506680c55c9a18980b2998c24d89d477721
Artem <2urnix@gmail.com>
2024-04-18 22:00:17 +0300

    fix: detect params

--- clean-disk.sh
+++ clean-disk.sh
@@ -12,40 +12,49 @@ duplicates_dir="$2"
 stat_mode=false
 declare -a exclude_patterns
 
-while getopts "se:" opt; do
-  case $opt in
-    s) stat_mode=true ;;
-    e) exclude_patterns+=("$OPTARG") ;;
-    \?) echo "Invalid option -$OPTARG" >&2; exit 1 ;;
+shift 2  # Shift past the first two mandatory arguments
+
+while (( "$#" )); do
+  case "$1" in
+    -s)
+      stat_mode=true
+      shift
+      ;;
+    -e)
+      exclude_patterns+=("$2")
+      shift 2
+      ;;
+    *)
+      echo "Invalid option $1" >&2
+      exit 1
+      ;;
   esac
 done
 
+
+# Combine exclude patterns into one regex
+exclude_regex=$(IFS='|'; echo "${exclude_patterns[*]}")
+
+echo "Stat mode: $stat_mode"
+echo "Excluded paths: ${exclude_patterns[*]}"
+
+
+
+
 mkdir -p "$duplicates_dir"
 
-echo $stat_mode
 
 fdupes -rn "$search_dir" > duplicates.txt
 
 if [ "$stat_mode" = true ]; then
     echo "Statistics mode: Displaying duplicate file statistics only."
-    if [ -n "$exclude_pattern" ]; then
-        awk -v pat="$exclude_pattern" '$0 !~ pat {if ($0 !~ /^$/){if (prev != $0) skip=1; if (skip) print $0;} else skip=0; prev=$0}' duplicates.txt | sort | uniq -c
-    else
-        awk 'NF{if ($0 !~ /^$/){if (prev != $0) skip=1; if (skip) print $0;} else skip=0; prev=$0}' duplicates.txt | sort | uniq -c
-    fi
-else
-    echo "Moving duplicate files to: $duplicates_dir"
-    if [ -n "$exclude_pattern" ]; then
-        awk -v pat="$exclude_pattern" '$0 !~ pat {if ($0 !~ /^$/){if (prev != $0) skip=1; if (skip) print $0;} else skip=0; prev=$0}' duplicates.txt | \
-        while IFS= read -r file; do
-            mv "$file" "$duplicates_dir" 2>>too_long.txt || echo "$file" >> too_long.txt
-        done
-    else
-        awk 'NF{if ($0 !~ /^$/){if (prev != $0) skip=1; if (skip) print $0;} else skip=0; prev=$0}' duplicates.txt | \
-        while IFS= read -r file; do
-            mv "$file" "$duplicates_dir" 2>>too_long.txt || echo "$file" >> too_long.txt
-        done
-    fi
+    awk -v pat="$exclude_regex" '$0 !~ pat && NF{if ($0 !~ /^$/){if (prev != $0) skip=1; if (skip) print $0;} else skip=0; prev=$0}' duplicates.txt | sort | uniq -c
+#else
+#    echo "Moving duplicate files to: $duplicates_dir"
+#    awk -v pat="$exclude_regex" '$0 !~ pat && NF{if ($0 !~ /^$/){if (prev != $0) skip=1; if (skip) print $0;} else skip=0; prev=$0}' duplicates.txt | \
+#    while IFS= read -r file; do
+#        mv "$file" "$duplicates_dir" 2>>too_long.txt || echo "$file" >> too_long.txt
+#    done
 fi
 
 echo "Done"
